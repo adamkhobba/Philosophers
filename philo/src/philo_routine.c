953806fb2b11925493519892885d864ba5952a8b
Adam Khobba <akhobba@c3r9p2.1337.ma>
2024-08-07 11:18:09 +0100

    norm

--- philo/src/philo_routine.c
+++ philo/src/philo_routine.c
@@ -6,18 +6,25 @@
 /*   By: akhobba <akhobba@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 21:26:03 by akhobba           #+#    #+#             */
-/*   Updated: 2024/08/07 10:49:38 by akhobba          ###   ########.fr       */
+/*   Updated: 2024/08/07 11:03:56 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+void ft_print_msg(t_philo *data, char *status, char *color)
+{
+		pthread_mutex_lock(&data->data->lock_print);
+		printf("%s %zu %d %s\n%s", color, get_current_time()
+			- data->start_time, data->index_of_philo, status, NC);
+		pthread_mutex_unlock(&data->data->lock_print);
+}
+
 void	ft_eat_v2(t_philo *data)
 {
 	pthread_mutex_lock(&data->forks);
 	if (get(&data->locker, &data->dead))
-		printf("%s %zu %d has taken a fork\n%s", BLUE, get_current_time()
-			- data->start_time, data->index_of_philo, NC);
+		ft_print_msg(data, "has taken a fork", BLUE);
 	if (data->data->num_of_philos == 1)
 	{
 		pthread_mutex_unlock(&data->forks);
@@ -26,11 +33,9 @@ void	ft_eat_v2(t_philo *data)
 	pthread_mutex_lock(data->forks_l);
 	set(&data->locker, &data->last_meal, get_current_time());
 	if (get(&data->locker, &data->dead))
-		printf("%s %zu %d has taken a fork\n%s", BLUE, get_current_time()
-			- data->start_time, data->index_of_philo, NC);
+		ft_print_msg(data, "has taken a fork", BLUE);
 	if (get(&data->locker, &data->dead))
-		printf("%s %zu %d is eating\n%s", CYAN, get_current_time()
-			- data->start_time, data->index_of_philo, NC);
+		ft_print_msg(data, "is eating", CYAN);
 	set(&data->locker, &data->full, data->full + 1);
 	ft_usleep(data->data->time_to_eat);
 	pthread_mutex_unlock(data->forks_l);
@@ -41,8 +46,7 @@ void	ft_eat(t_philo *data)
 {
 	pthread_mutex_lock(data->forks_l);
 	if (get(&data->locker, &data->dead))
-		printf("%s %zu %d has taken a fork\n%s", BLUE, get_current_time()
-			- data->start_time, data->index_of_philo, NC);
+		ft_print_msg(data, "has taken a fork", BLUE);
 	if (data->data->num_of_philos == 1)
 	{
 		pthread_mutex_unlock(data->forks_l);
@@ -51,11 +55,9 @@ void	ft_eat(t_philo *data)
 	pthread_mutex_lock(&data->forks);
 	set(&data->locker, &data->last_meal, get_current_time());
 	if (get(&data->locker, &data->dead))
-		printf("%s %zu %d has taken a fork\n%s", BLUE, get_current_time()
-			- data->start_time, data->index_of_philo, NC);
+		ft_print_msg(data, "has taken a fork", BLUE);
 	if (get(&data->locker, &data->dead))
-		printf("%s %zu %d is eating\n%s", CYAN, get_current_time()
-			- data->start_time, data->index_of_philo, NC);
+		ft_print_msg(data, "is eating", CYAN);
 	set(&data->locker, &data->full, data->full + 1);
 	ft_usleep(data->data->time_to_eat);
 	pthread_mutex_unlock(data->forks_l);
@@ -77,24 +79,15 @@ void	*ft_philos_routine(void *args)
 			ft_eat(data);
 		else
 			ft_eat_v2(data);
-		if (data->data->num_of_philos == 1)
+		usleep(60 * data->data->num_of_philos);
+		if (!get(&data->locker, &data->data->dead_flag)
+			||data->data->num_of_philos == 1)
 			break ;
-		if (!get(&data->locker, &data->dead))
-			break ;
-		usleep(100 * data->data->num_of_philos);
-		if (!get(&data->locker, &data->data->dead_flag))
-			break ;
-		pthread_mutex_lock(&data->data->lock_print);
-		printf("%s %zu %d is sleeping\n%s", YELLOW, get_current_time()
-			- data->start_time, data->index_of_philo, NC);
-		pthread_mutex_unlock(&data->data->lock_print);
 		ft_usleep(data->data->time_to_sleep);
 		if (!get(&data->locker, &data->dead))
 			break ;
-		pthread_mutex_lock(&data->data->lock_print);
-		printf("%s %zu %d is thinking%s\n", GREEN, (get_current_time()
-					- data->start_time), data->index_of_philo, NC);
-		pthread_mutex_unlock(&data->data->lock_print);
+		ft_print_msg(data, "is sleeping", YELLOW);
+		ft_print_msg(data, "is thinking", GREEN);
 		usleep(499);
 	}
 	return (NULL);
